bea0fe5cd9a1389c19e0ea389b840a81336b43c5
tomishima <pumoco2904@icloud.com>
2023-06-01 07:22:41 +0900

    feat: #8 extract_sentenceの仕様を大幅に変更

--- src/utils/extract_sentences.py
+++ src/utils/extract_sentences.py
@@ -1,76 +1,58 @@
 import csv
 import gzip
-import json
-from tqdm import tqdm
+import random
+import datetime
 
 import file_handlers as fh
 from extract_entity import extract_entity
 
 
-def _extract_sentences_from_corpus(head: str, tail: str, corpus: list, num_extract=3) -> list:
-    sentences = []
-    for sentence in corpus:
-        if head in sentence:
-            if tail in sentence:
-                sentences.append(sentence)
-                if len(sentences) == num_extract:
-                    break
-    return sentences
-
-
 def extract_sentences(
         output_path: str,
         corpus: list,
-        head_and_tail: list,
-        lang: str = "ja") -> None:
-    # ConceptNet から unique な (head, tail) のペアを抽出
-    unique_pairs = set([])
-    for pair in head_and_tail:
-        # URI から entity を抽出
-        head_entity = extract_entity(lang, pair[0])
-        tail_entity = extract_entity(lang, pair[1])
+        conceptnet: list,
+        num_extract: int = 5) -> None:
+    with gzip.open(output_path, "wt") as f:
+        writer = csv.writer(f)
+        i = 0
+        for row in conceptnet:
+            # URI から entity を抽出
+            head = row[1]
+            tail = row[2]
 
-        if head_entity == None or tail_entity == None:
-            continue
+            sentences = []
+            for sentence in corpus:
+                if head in sentence and tail in sentence:
+                    sentences.append(sentence)
+                    if len(sentences) == num_extract:
+                        break
 
-        # 例. "イタリアン_コーヒー" -> "イタリアンコーヒー"
-        if lang == "ja":
-            head_entity = head_entity.replace("_", "")
-            tail_entity = tail_entity.replace("_", "")
-        # 例. "New_York" -> "New York"
-        else:
-            head_entity = head_entity.replace("_", " ")
-            tail_entity = tail_entity.replace("_", " ")
-        unique_pairs.add((head_entity, tail_entity))
+            data = (row[0], head, tail, sentences)
+            writer.writerow(data)
 
-    sentences_dict_corpus = {}
-
-    for pair in tqdm(unique_pairs, total=len(unique_pairs)):
-        head_entity = pair[0]
-        tail_entity = pair[1]
-
-        # head entity が キーになければ追加
-        if head_entity not in sentences_dict_corpus:
-            sentences_dict_corpus[head_entity] = {}
-
-        # tail entity が キーになければ追加して、文をコーパスから抽出
-        if not tail_entity in sentences_dict_corpus[head_entity]:
-            sentences_dict_corpus[head_entity][tail_entity] = \
-            _extract_sentences_from_corpus(head_entity, tail_entity, corpus)
-
-    with open(output_path, "w") as f:
-        json.dump(sentences_dict_corpus, f, ensure_ascii=False)
+            i += 1
+            if i % 1000 == 0:
+                print(f"{datetime.datetime.now()}: {i} triplets have been processed.")
     print(f"Successfully dumped {output_path} !")
 
 
 if __name__ == "__main__":
+    # ConceptNetをロード
+    lang = "ja"
+    dataset_type = "train"
+    dataset_dir = f"datasets/conceptnet-assertions-5.7.0/{lang}"
+    input_file = f"{dataset_type}_triplets.csv"
+    conceptnet_path = f"{dataset_dir}/{input_file}"
+    conceptnet = fh.read_csv(conceptnet_path, has_header=False)
+
     # 日本語Wikipediaをロード
     corpus_dir = "datasets/jawiki-20221226"
-    input_corpus_path = f"{corpus_dir}/train_1.9M.txt.gz"
+    input_corpus_path = f"{corpus_dir}/train_sentences_700.txt.gz"
 
     print("Loading wikipedia corpus ...")
     with gzip.open(input_corpus_path, "rt") as f:
         corpus = f.readlines()
+    random.shuffle(corpus)
 
     # # STAIR Captionsをロード
     # corpus_dir = "datasets/STAIR-captions"
@@ -80,21 +62,9 @@ if __name__ == "__main__":
     # with open(input_corpus_path, "r") as f:
     #     corpus = f.readlines()
 
-    # ConceptNetをロード
-    lang = "ja"
-    dataset_dir = f"datasets/conceptnet-assertions-5.7.0/{lang}"
-    input_file = f"conceptnet-assertions-5.7.0_{lang}.csv.gz"
-    conceptnet_path = f"{dataset_dir}/{input_file}"
+    output_corpus_path = f"{corpus_dir}/extracted_corpus_{dataset_type}.csv.gz"
 
-    conceptnet = []
-    with gzip.open(conceptnet_path, 'rt') as f:
-        reader = csv.reader(f, delimiter='\t')
-        for row in reader:
-            conceptnet.append(row[2:-1])  # head と tail だけ取得
-
-    fh.makedirs(corpus_dir)
-    output_corpus_path = f"{corpus_dir}/extracted_corpus.json"
-
+    print("Extracting sentences ...")
     extract_sentences(output_path=output_corpus_path,
                       corpus=corpus,
-                      head_and_tail=conceptnet)
+                      conceptnet=conceptnet)
