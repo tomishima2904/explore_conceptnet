60b004dbcb60da626a7a0ee33b39d19810e698af
tomishima <pumoco2904@icloud.com>
2023-06-09 14:16:50 +0900

    fix: 文中の「」内の。は無視するようにした

--- src/utils/preprocess_wiki.py
+++ src/utils/preprocess_wiki.py
@@ -2,6 +2,7 @@ import gzip
 from tqdm import tqdm
 import datetime
 import sys
+import re
 
 from normalize_neologd import normalize_neologd, subtract_hashtag
 
@@ -14,22 +15,38 @@ print(f"Loading {input_corpus_path}")
 with gzip.open(input_corpus_path, "rt") as f:
     wiki_corpus = f.readlines()
 
+# クォーテーションマークで囲まれた部分を抽出する正規表現パターン
+pattern = re.compile(r'「.+?」')
+
 print(f"Dumping {output_corpus_path}")
 sys.stdout.flush()
 with gzip.open(output_corpus_path, "wt") as f:
-    i = 0
-    for article in wiki_corpus:
-        i += 1
+    for i, article in enumerate(wiki_corpus):
         if i % 10000 == 0:
             print(f"{datetime.datetime.now()}: {i} articles has been processed.")
             sys.stdout.flush()
-        sentence_list = subtract_hashtag(normalize_neologd(article)).split("。")
-        f.write("\n".join(sentence_list))
+
+        # 正規化
+        normalized_article = subtract_hashtag(normalize_neologd(article))
+
+        # クォーテーションマークで囲まれた部分を一時的に置換しておく
+        replaced_article = pattern.sub(lambda x: x.group().replace("。", "＠＠＠"), normalized_article)
+
+        # 「。」で改行する
+        sentences = replaced_article.split("。")
+        formatted_text = "\n".join(sentence.replace("＠＠＠", "。").strip() for sentence in sentences if sentence != "")
+
+        # 文末の「。」を削除する
+        formatted_text = formatted_text.rstrip("。")
+
+        # クォーテーションマークで囲まれた部分を元に戻す
+        sentence_list = pattern.sub(lambda x: x.group().replace("＠＠＠", "。"), formatted_text)
+        f.write(sentence_list)
 
 print(f"Successfully dumped {output_corpus_path}")
 
-limit = 1000
-output_1000_corpus_path = f"{corpus_dir}/jawiki_sentences_1000.txt.gz"
+limit = 200
+output_1000_corpus_path = f"{corpus_dir}/jawiki_sentences_200.txt.gz"
 with gzip.open(output_corpus_path, "rt") as rf:
     with gzip.open(output_1000_corpus_path, "wt") as wf:
         for line in rf:
