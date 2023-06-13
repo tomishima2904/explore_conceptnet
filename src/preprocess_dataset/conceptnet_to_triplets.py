24c8c5aeac1ac2b7c173dde580aafebbe81f0ded
tomishima <pumoco2904@icloud.com>
2023-06-13 10:13:35 +0900

    feat: conceptnetからトリプレットを形成する際に除去されたものも記録

--- src/preprocess_dataset/conceptnet_to_triplets.py
+++ src/preprocess_dataset/conceptnet_to_triplets.py
@@ -7,8 +7,11 @@ from normalize_neologd import normalize_neologd
 
 lang = "ja"
 dataset_dir = f"datasets/conceptnet-assertions-5.7.0/{lang}"
-input_file = f"conceptnet-assertions-5.7.0_{lang}.csv.gz"
-conceptnet_path = f"{dataset_dir}/{input_file}"
+input_path = f"conceptnet-assertions-5.7.0_{lang}.csv.gz"
+output_dir = dataset_dir
+output_path = f"{output_dir}/origin_triplets.csv"
+removed_path = f"{output_dir}/removed_triplets.csv.gz"
+conceptnet_path = f"{dataset_dir}/{input_path}"
 
 conceptnet = []
 with gzip.open(conceptnet_path, 'rt') as f:
@@ -27,38 +30,38 @@ relation_mapping = {row[0]: row[2] for row in relations_data}
 
 all_data = {relation_uri: [] for relation_uri in relation_mapping.keys()}
 
-for row in conceptnet:
-    relation_uri = row[0]
+with gzip.open(removed_path, 'wt') as f:
+    writer = csv.writer(f)
+    for i, row in enumerate(conceptnet):
+        relation_uri = row[0]
 
-    # `/r/ExternalURL` はデータセットに含めない
-    if relation_uri == "/r/ExternalURL":
-        continue
+        # `/r/ExternalURL` はデータセットに含めない
+        if relation_uri == "/r/ExternalURL":
+            continue
 
-    # `NotUsedFor`などの否定的な関係性は含めない
-    if "Not" in relation_uri:
-        continue
+        # `NotUsedFor`などの否定的な関係性は含めない
+        if "Not" in relation_uri:
+            continue
 
-    head = extract_entity(lang, row[1])
-    tail = extract_entity(lang, row[2])
-    if head == None or tail == None:
-        continue
+        head = extract_entity(lang, row[1])
+        tail = extract_entity(lang, row[2])
+        if head == None or tail == None:
+            writer.writerow((i, *row[:]))
+            continue
 
-    # 正規化
-    head = normalize_neologd(head)
-    tail = normalize_neologd(tail)
-    # 例. "イタリアン_コーヒー" -> "イタリアンコーヒー"
-    if lang == "ja":
-        head = head.replace("_", "")
-        tail = tail.replace("_", "")
-    # 例. "New_York" -> "New York"
-    else:
-        head = head.replace("_", " ")
-        tail = tail.replace("_", " ")
+        # 正規化
+        head = normalize_neologd(head)
+        tail = normalize_neologd(tail)
+        # 例. "イタリアン_コーヒー" -> "イタリアンコーヒー"
+        if lang == "ja":
+            head = head.replace("_", "")
+            tail = tail.replace("_", "")
+        # 例. "New_York" -> "New York"
+        else:
+            head = head.replace("_", " ")
+            tail = tail.replace("_", " ")
 
-    all_data[relation_uri].append([head, tail])
-
-output_dir = dataset_dir
-output_path = f"{output_dir}/origin_triplets.csv"
+        all_data[relation_uri].append([head, tail])
 
 with open(output_path, 'w') as f:
     writer = csv.writer(f)
@@ -66,4 +69,4 @@ with open(output_path, 'w') as f:
         # (relation, head, tail) のトリプレットを作成
         triplets = [[relation_mapping[relation_uri], pair[0], pair[1]]
                     for pair in heads_and_tails]
-        writer.writerow(triplets)
+        writer.writerows(triplets)
