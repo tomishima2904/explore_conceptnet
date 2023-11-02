226607cf47c1fd891731cb50b9a28191a75c249f
tomishima <pumoco2904@icloud.com>
2023-11-02 11:15:27 +0900

    chore: 実行ファイルを修正

--- do_1.sh
+++ do_1.sh
@@ -1,6 +1,14 @@
-SEED=8
-# python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  --device_type=cuda:1 --num_refs=0 --template_name=zero-shot_no_refs_3 --seed=$SEED
-python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  --device_type=cuda:1 --num_refs=3 --template_name=zero-shot_with_refs_3 --seed=$SEED
-# python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  --device_type=cuda:1 --num_refs=30 --template_name=zero-shot_with_refs_3 --seed=$SEED
-SEED=10
-python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  --device_type=cuda:1 --num_refs=3 --template_name=zero-shot_with_refs_3 --seed=$SEED
+SEED=1
+python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py' \
+        --device_type=cuda:1 \
+        --model=matsuo-lab/weblab-10b \
+        --num_return_sequences=30 \
+        --num_refs=0 \
+        --template_name=zero-shot_no_refs_5 \
+        --seed=$SEED \
+        --input_path="datasets/連想語頻度表/all/dev_mini2/htrkpns_tmp.csv.gz"
+
+# rinna/japanese-gpt-neox-3.6b
+# cyberagent/open-calm-7b
+# matsuo-lab/weblab-10b
+# pfnet/plamo-13b
