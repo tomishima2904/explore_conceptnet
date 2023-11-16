11820e36f2f40560a267967937ead2c1a835a7b7
tomishima <pumoco2904@icloud.com>
2023-11-16 15:51:57 +0900

    chore

--- do_1.sh
+++ do_1.sh
@@ -1,12 +1,11 @@
-SEED=1
-python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py' \
+# テスト用の連想理由の説明（参照文無し）
+python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  \
         --device_type=cuda:1 \
         --model=matsuo-lab/weblab-10b \
-        --num_return_sequences=30 \
+        --num_return_sequences=3 \
         --num_refs=0 \
-        --template_name=zero-shot_no_refs_5 \
-        --seed=$SEED \
-        --input_path="datasets/連想語頻度表/all/dev_mini2/htrkpns_tmp.csv.gz"
+        --template_name=few-shot_no_refs_5 \
+        --seed=$SEED
 
 # rinna/japanese-gpt-neox-3.6b
 # cyberagent/open-calm-7b
