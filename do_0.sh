80be2ebe380f305f94157d52cfe33b2196f89769
tomishima <pumoco2904@icloud.com>
2023-12-14 09:28:04 +0900

    test: 実験終了

--- do_0.sh
+++ do_0.sh
@@ -1,12 +1,13 @@
 SEED=1
 # テスト用の連想理由の説明（参照文無し）
-# python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  \
-#         --device_type=cuda:0 \
-#         --model=matsuo-lab/weblab-10b \
-#         --num_return_sequences=3 \
-#         --num_refs=0 \
-#         --template_name=few-shot_no_refs_5 \
-#         --seed=$SEED
+python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  \
+        --device_type=cuda:0 \
+        --model=matsuo-lab/weblab-10b \
+        --num_return_sequences=3 \
+        --num_refs=0 \
+        --template_dir=datasets/連想語頻度表/templates \
+        --template_name="10-shot_len_xRandom" \
+        --seed=$SEED
 
 # テスト用の連想理由の説明（参照文有り for ICNLSP2023）
 # python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  \
@@ -16,15 +17,15 @@ SEED=1
 #         --template_name=zero-shot_no_refs_5 \
 #         --seed=$SEED
 
-# Few-shot用（訓練用）の連想理由の説明
-python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py' \
-        --device_type=cuda:0 \
-        --model=matsuo-lab/weblab-10b \
-        --num_return_sequences=30 \
-        --num_refs=0 \
-        --template_name=zero-shot_no_refs_5 \
-        --seed=$SEED \
-        --input_path="datasets/連想語頻度表/all/dev_mini2/htrkpns_tmp.csv.gz"
+# # Few-shot用（訓練用）の連想理由の説明
+# python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py' \
+#         --device_type=cuda:0 \
+#         --model=matsuo-lab/weblab-10b \
+#         --num_return_sequences=30 \
+#         --num_refs=0 \
+#         --template_name=zero-shot_no_refs_5 \
+#         --seed=$SEED \
+#         --input_path="datasets/連想語頻度表/all/dev_mini2/htrkpns_tmp.csv.gz"
 
 # rinna/japanese-gpt-neox-3.6b
 # cyberagent/open-calm-7b
--- do_1.sh
+++ do_1.sh
@@ -1,10 +1,12 @@
+SEED=2
 # テスト用の連想理由の説明（参照文無し）
 python '/work/tomishima2904/explore_conceptnet/src/models/text_generation_model.py'  \
         --device_type=cuda:1 \
         --model=matsuo-lab/weblab-10b \
         --num_return_sequences=3 \
         --num_refs=0 \
-        --template_name=few-shot_no_refs_5 \
+        --template_dir=datasets/連想語頻度表/templates/5-shot_len_deduplicated_random \
+        --template_name="5-shot_len_deduplicated_random_$SEED" \
         --seed=$SEED
 
 # rinna/japanese-gpt-neox-3.6b
