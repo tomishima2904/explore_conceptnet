a9d92be23b95167da40d347caa9163b0909a07ac
tomishima <pumoco2904@icloud.com>
2024-02-07 09:43:30 +0900

    readme: 生成から評価までのやり方を追記

--- do_3.sh
+++ do_3.sh
@@ -19,7 +19,15 @@ python '/work/tomishima2904/explore_conceptnet/src/models/gen_with_mlm_selection
 # - 5-shot_len_mR: 手動1 (富島)
 # - 5-shot_len_mT: 手動2 (問井)
 # - 5-shot_len_mY: 手動3 (悠真)
-
-# 出力結果は `results/ja/連想語頻度表/{dir_name}` に保存
 # 実験に使うtemplateは `datasets/連想語頻度表/templates/master30` にある
 # 上記の `{template_name}.json` が `template_dir` に保存されている
+
+# 生成は 各種パラメータを定めたら，`./do_3.sh` をするだけ（全部終わるのに30分くらいかかるかも）
+
+# 出力結果は `results/ja/連想語頻度表/{dir_name}` に保存
+# formmated_results.txt で二値評価を行ったら，必ずそのファイル名を `formmated_results_.txt` と変える
+# 名称変更した後，`src/utils/result_extractor.ipynb` の `Labels.txt` という項目で，
+# `result_dir` を実験結果が保存されているディレクトリ名に指定して，セルを実行すれば `labels.txt` が出力される
+# `label.txt` は `formmated_results_.txt` の結果をエクセル等に貼りやすく集計した結果が保存されている
+# `labels.txt` は 刺激語と連想語の組の数の長さの列ベクトルが出力されるので，エクセル等に貼り付けて使える
+
